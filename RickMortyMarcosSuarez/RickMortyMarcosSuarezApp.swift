936f1a117a22b39063ff911650ef1b73d5dd881a
Marcos Suárez <marcos.sua.aya@gmail.com>
2024-04-16 09:21:06 +0200

    Initial Commit

---
+++ RickMortyMarcosSuarez/RickMortyMarcosSuarezApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyMarcosSuarezApp.swift
+//  RickMortyMarcosSuarez
+//
+//  Created by Marcos Suarez Ayala on 16/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyMarcosSuarezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
