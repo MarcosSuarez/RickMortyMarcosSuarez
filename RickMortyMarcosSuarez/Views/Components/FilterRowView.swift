9695a01f4b4eee0c8eb3712c4a2f9f0421a88d82
Marcos <marcos.suarezayala@sfy.com>
2025-02-09 06:34:28 +0100

    filter and colors added in FilterRowView.

--- RickMortyMarcosSuarez/Views/Components/FilterRowView.swift
+++ RickMortyMarcosSuarez/Views/Components/FilterRowView.swift
@@ -12,6 +12,7 @@ struct FilterRowView: View {
     var title: String? = nil
     var items: [String] = []
     @Binding var selectedItem: String
+    var backgroundColor:(selected: Color, unSelected: Color) = (.green, .gray.opacity(0.2))
     
     var body: some View {
         VStack(alignment: .leading, spacing: 4) {
@@ -21,13 +22,13 @@ struct FilterRowView: View {
             }
             ScrollView(.horizontal) {
                 HStack(spacing: 4) {
-                    ForEach(items, id: \.self) { item in
+                    ForEach(getItems(), id: \.self) { item in
                         Text(item.capitalized)
                             .font(.callout)
-                            .frame(minWidth: 30)
-                            .padding(.vertical, 4)
+                            .frame(minWidth: 45)
+                            .padding(.vertical, 6)
                             .padding(.horizontal, 10)
-                            .background(item == selectedItem ? .green : .gray.opacity(0.2))
+                            .background(getBackgroundColor(item: item))
                             .foregroundColor(item == selectedItem ? .white : .secondary)
                             .cornerRadius(16)
                             .onTapGesture {
@@ -45,11 +46,36 @@ struct FilterRowView: View {
         }
         .frame(maxWidth: .infinity, alignment: .leading)
     }
+    
+    private func getItems() -> [String] {
+        items.filter { !$0.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty }
+    }
+    
+    private func getBackgroundColor(item: String) -> Color {
+        item == selectedItem ? backgroundColor.selected : backgroundColor.unSelected
+    }
 }
 
 #Preview {
-    FilterRowView(title: "Gender By:",
-                  items: (Gender.allCases.compactMap{ $0.rawValue }),
-                  selectedItem: .constant(Gender.unknown.rawValue)
-    )
+    // Xcode 16
+    // @Previewable @State var selectedItem = "K"
+    struct PreviewWrapper: View {
+        @State var selectedItem = "☠️"
+        private let items = [" ", "🕘", "\n", " 🫸🏼 🫷🏽 ", "😎", "☠️", "", "👤", "   ", "👥"]
+        private let backgroundColors:(Color, Color) = (.red, .green.opacity(0.4))
+        
+        var body: some View {
+            VStack {
+                FilterRowView(title: "Select One:",
+                              items: items,
+                              selectedItem: $selectedItem,
+                              backgroundColor: backgroundColors)
+                
+                FilterRowView(title: "Gender By:",
+                              items: (Gender.allCases.compactMap{ $0.rawValue }),
+                              selectedItem: .constant(Gender.unknown.rawValue))
+            }
+        }
+    }
+    return PreviewWrapper()
 }
